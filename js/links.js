55ffb8027482042fcb889075733d99252108cb00
AniruddhaSoni <anirudh.soni007@gmail.com>
2022-08-18 00:36:48 +0530

    first commit

---
+++ js/links.js
@@ -0,0 +1,32 @@
+export const links = [
+  {
+    name: "Whatsapp",
+    link: "https://example.com",
+    image: "./assets/whatsapp.png",
+  },
+  {
+    name: "Twitter",
+    link: "https://example.com",
+    image: "./assets/twitter.png",
+  },
+  {
+    name: "Linkedin",
+    link: "https://example.com",
+    image: "./assets/linkedin.png",
+  },
+  {
+    name: "Instagram",
+    link: "https://example.com",
+    image: "./assets/instagram.png",
+  },
+  {
+    name: "Discord",
+    link: "https://example.com",
+    image: "./assets/discord.png",
+  },
+  {
+    name: "E-mail",
+    link: "https://example.com",
+    image: "./assets/gmail.png",
+  },
+];
