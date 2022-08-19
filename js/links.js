1ce346b8fba4b61e192ef96a2328b461fc5bf676
Diya Vijay <87236107+DiyaVj@users.noreply.github.com>
2022-08-19 21:08:59 +0530

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,22 +1,22 @@
 export const links = [
   {
     name: "Whatsapp",
-    link: "https://example.com",
+    link: "https://chat.whatsapp.com/H37W8w8UjnB4sje9kFAkVO",
     image: "./assets/whatsapp.png",
   },
   {
     name: "Twitter",
-    link: "https://example.com",
+    link: "https://twitter.com/gfg_rtu?t=ylCa3VY8bL0YkHMAD_TT0Q&s=08",
     image: "./assets/twitter.png",
   },
   {
     name: "Linkedin",
-    link: "https://example.com",
+    link: "https://www.linkedin.com/company/gfg-rtu/about/",
     image: "./assets/linkedin.png",
   },
   {
     name: "Instagram",
-    link: "https://example.com",
+    link: "https://www.instagram.com/gfg_rtu/",
     image: "./assets/instagram.png",
   },
   {
@@ -26,12 +26,12 @@ export const links = [
   },
   {
     name: "E-mail",
-    link: "https://example.com",
+    link: "mail to: gfgstudentchapter.rtu@gmail.com",
     image: "./assets/gmail.png",
   },
   {
     name: "Github",
-    link: "https://example.com",
+    link: "https://github.com/GFGSC-RTU",
     image: "./assets/github.png",
   },
 ];
